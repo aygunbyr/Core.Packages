0ee53b6b51cca8783c71a2912d6c8126cd281e70
Aygün Bayır <turkoaygun@gmail.com>
2024-12-11 17:36:52 +0300

    MsSql Logging implementation

--- Core.CrossCuttingConcerns/Serilog/ConfigurationModels/FileLogConfiguration.cs
+++ Core.CrossCuttingConcerns/Serilog/ConfigurationModels/FileLogConfiguration.cs
@@ -14,24 +14,3 @@ public class FileLogConfiguration
         FolderPath = folderPath;
     }
 }
-
-public class MsSqlConfiguration
-{
-    public string ConnectionString { get; set; }
-    public string TableName { get; set; }
-    public bool AutoCreateSqlTable { get; set; }
-
-    public MsSqlConfiguration()
-    {
-        ConnectionString = string.Empty;
-        TableName = string.Empty;
-    }
-
-    public MsSqlConfiguration(string connectionString, string tableName, bool autoCreateTable)
-    {
-        ConnectionString = connectionString;
-        TableName = tableName;
-        AutoCreateSqlTable = autoCreateTable;
-
-    }
-}
